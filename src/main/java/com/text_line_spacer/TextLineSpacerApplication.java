0c7c3245f37552f0c0fba4cd75dd461c87ee91c5
Mehmet Ali <mehmetalikaradag@ogrenci.karabuk.edu.tr>
2025-11-09 13:11:35 +0100

    Add files via upload
    initial via upload

---
+++ src/main/java/com/text_line_spacer/TextLineSpacerApplication.java
@@ -0,0 +1,13 @@
+package com.text_line_spacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextLineSpacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextLineSpacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/text_line_spacer/TextLineSpacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.text_line_spacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextLineSpacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
